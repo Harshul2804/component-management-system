a5458c8073bd8300fb583c0e8b4d72b289898e16
harshul2804 <harshul2804@gmail.com>
2020-08-30 00:18:01 +0530

    added credentials for clearDB-MySQL for heroku

--- includes/db.php
+++ includes/db.php
@@ -3,8 +3,9 @@
 // $db['db_user']='root';
 // $db['db_pass']='';
 // $db['db_name']='cms';
-
-$db = array("db_host"=>"localhost", "db_user"=>"root", "db_pass"=>"", "db_name"=>"cms");
+//mysql://b87e69e77c65ed:2645acde@us-cdbr-east-02.cleardb.com/heroku_2ebed0234ea56fd?reconnect=true
+//$db = array("db_host"=>"localhost", "db_user"=>"root", "db_pass"=>"", "db_name"=>"cms");
+$db = array("db_host"=>"us-cdbr-east-02.cleardb.com", "db_user"=>"b87e69e77c65ed", "db_pass"=>"2645acde", "db_name"=>"heroku_2ebed0234ea56fd");
 
 foreach($db as $key => $value){
     define(strtoupper($key),$value);
